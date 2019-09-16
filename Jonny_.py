bc1a1874a2265ee6d70e5b558ab2b457e75df8e1
Aeriel Denmark <49964335+aidenmark@users.noreply.github.com>
2019-09-15 17:22:18 -0700

    Question Answered
    - this commit answers the question about what 'path.expanduser' is

---
+++ Jonny_.py
@@ -0,0 +1,16 @@
+# Aeriel Denmark
+# Jonny Dailey
+# #000Dev
+# Sat 9/14/2019
+
+import os
+
+path = os.path.expanduser("~/Desktop")
+
+#tell me what this is and what it does. "path.expanduser"
+
+# "path.expanduser" is used to expand an initial path to the user’s home directory
+
+os.chdir(path)
+
+os.mkdir("Ava Rose The Boys Stink")
