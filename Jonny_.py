4ed3d2a360481311c924cccf75b1f6b3df1b1b02
Aeriel Denmark <aidenmark1@icloud.com>
2019-09-24 22:28:40 -0700

    Why doesn't this work?
    I defined a variable and then called it ... I'm telling it to do the exact same thing it did with 'currentDT' ... I don't get it

--- Jonny_.py
+++ Jonny_.py
@@ -8,7 +8,7 @@ import datetime
 import os.path
 import shutil
 
-# 'shutil' helps to automate copying files and directories; saves the steps of opening, reading, 
+# 'shutil' helps to automate copying files and directories; saves the steps of opening, reading,
 # writing and closing files when there is no actual processing
 
 path = os.path.expanduser("~/Desktop")
@@ -18,19 +18,30 @@ w = "Wakanda Forever/"
 
 currentDT = datetime.datetime.now().strftime("%m/%d/%Y - %H:%M")
 
+template = """
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+</body>
+</html>
+"""
+
 if os.path.exists(w):
-    print "It exists"
+    print("It exists")
     os.chdir(w) #change user's directory to wanted directory
-    print cwd #necessary for trobleshooting purposes
+    print(cwd) #necessary for trobleshooting purposes
     f = open("index.html", "w+")
     f.write(currentDT)
+    f.write(template) #why doesn't this work ???!!!
     f.close()
 else:
-    print "It does not exist."
-    print ""
+    print("It does not exist.")
+    print("")
     os.mkdir(w) #make the directory on the user's desktop
     os.chdir(w) #change directory to newly created directory on user's desktop
-    print "Directory Created." #necessary checkpoint in troubleshooting process
+    print("Directory Created.") #necessary checkpoint in troubleshooting process
     f = open("index.html", "w+")
     f.write(currentDT)
+    f.write(template) #why doesn't this work ???!!!
     f.close()
