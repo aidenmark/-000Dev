e3cd2b8e2a7e21e2250dabc898a133547093e98b
Aeriel Denmark <aidenmark1@icloud.com>
2020-03-15 18:49:11 -0400

    Update character.py

--- character.py
+++ character.py
@@ -12,13 +12,3 @@ year = int(raw_input("What year is it?  "))
 future = int((year - age)+1000)
 
 print(name, "will be 1000 years old in the year", future)
-
-# How do I get the quotation marks and commas to not appear 
-# in the output?
-
-# Sample Solution from site
-
-# name = input("What is your name: ")
-# age = int(input("How old are you: "))
-# year = str((2014 - age)+100)
-# print(name + " will be 100 years old in the year " + year)
