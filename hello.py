62efbc0fa3af28ce14c4e519dd2c381af92b98d1
Aeriel Denmark <aeriel.denmark@gmail.com>
2022-08-21 11:11:56 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # The following is a function that defines the paramaters of 'say_hello'
 def say_hello(myParam):
-    print("Hello, {}World" .format(myParam))
+    print("Hello, World {}" .format(myParam))
 # {} is a placeholder for some number that will appear when the function
 # is called later on
 
