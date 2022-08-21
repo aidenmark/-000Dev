6514cf1b032fc1538c0a7cc90c9036c83595aada
Aeriel Denmark <aeriel.denmark@gmail.com>
2022-08-21 11:13:45 -0700

    character.py

--- character.py
+++ character.py
@@ -6,9 +6,9 @@
 
 import math
 
-name = str(raw_input("What is your name?  "))
-age =  int(raw_input("How old are you?  "))
-year = int(raw_input("What year is it?  "))
+name = str(input("What is your name?  "))
+age =  int(input("How old are you?  "))
+year = int(input("What year is it?  "))
 future = int((year - age)+1000)
 
 print(name, "will be 1000 years old in the year", future)
