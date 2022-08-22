060d88d21ec3abc9723f24e79864aa286f1cd7de
Aeriel Denmark <aeriel.denmark@gmail.com>
2022-08-21 17:08:49 -0700

    Create create_list.py

---
+++ create_list.py
@@ -0,0 +1,11 @@
+# Aeriel Denmark
+# List Creation
+
+# Write a program that prints out all the elements of 
+# a list that are less than 5.
+
+myList = [0, 1, 1.25, 2, 2.5, 3, 3.75, 5, 7, 9, 11, 13]
+
+for i in myList:
+	if i < 5:
+		print(i)
